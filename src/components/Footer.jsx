8ec5db23d31c3c5596d0e9f838f25edcee1a924f
MD Kamran Irshad <150000329+Mokamran5526@users.noreply.github.com>
2025-08-11 00:37:56 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+          <div className="col-md-6">
+            <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
+              <a  href="https://Kamran.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Md Kamran</a>
+            </p>
+            <a className="text-dark fs-4" href="https://github.com/Mokamran5526" target="_blank" rel="noreferrer">
+              <i className="fa fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
